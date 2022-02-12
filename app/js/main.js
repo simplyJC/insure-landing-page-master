6e3b7052e2e21532a247e3b1326decda16d2ee43
jcaltamia <jayceealtamia@gmail.com>
2022-02-12 19:28:20 +0800

    mobile layout

--- app/js/main.js
+++ app/js/main.js
@@ -3,13 +3,25 @@ const navClose = document.querySelector('.nav--close');
 const nav = document.querySelector('.header');
 
 navOpen.addEventListener('click', () => {
-  nav.style.height = '100%';
   navClose.style.display = 'block';
   navOpen.style.display = 'none';
+  nav.classList.toggle('header--active');
 });
 
 navClose.addEventListener('click', () => {
-  nav.style.height = '0%';
-  navOpen.style.display = 'block';
+  nav.classList.toggle('header--active');
   navClose.style.display = 'none';
+  navOpen.style.display = 'block';
+});
+
+window.addEventListener('resize', () => {
+  let viewportWidth = window.innerWidth;
+  if (viewportWidth >= 800 ) {
+    navClose.style.display = 'none';
+    navOpen.style.display = 'none';
+  }else{
+      navOpen.style.display = 'block';
+       //nav.classList.remove('header--active');
+       navClose.style.display = 'none';
+  }
 });
